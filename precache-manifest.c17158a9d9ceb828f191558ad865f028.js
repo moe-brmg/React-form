230940d38c4cf1f60a0253d8d766561c49b2bbac
Erfan-brmg <your_email_address@example.com>
2020-09-08 15:00:41 +0800

    Updates

---
+++ precache-manifest.c17158a9d9ceb828f191558ad865f028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24cc6958b747db7af4918e31bf04090",
+    "url": "/React-form/index.html"
+  },
+  {
+    "revision": "75596c6a28404aea648b",
+    "url": "/React-form/static/css/main.ce40afae.chunk.css"
+  },
+  {
+    "revision": "f61c10064650b29906af",
+    "url": "/React-form/static/js/2.5bbc10d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-form/static/js/2.5bbc10d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75596c6a28404aea648b",
+    "url": "/React-form/static/js/main.f867a382.chunk.js"
+  },
+  {
+    "revision": "08ac874124a5406619bb",
+    "url": "/React-form/static/js/runtime-main.74a3ba4e.js"
+  }
+]);
